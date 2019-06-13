4428eaf85a9cc7725b1acff69016bb5c24f635fe
Ulyana <ulyanakapeshina@gmail.com>
2019-06-13 13:50:37 +0300

    добавлена ф-я создания фрагмента, исправлены названия

--- js/setup.js
+++ js/setup.js
@@ -1,20 +1,20 @@
 'use strict';
-var setup = document.querySelector('.setup');
-setup.classList.remove('hidden');
-
-document.querySelector('.setup-similar').classList.remove('hidden');
-
+var setupElement = document.querySelector('.setup');
 var similarListElement = document.querySelector('.setup-similar-list');
 var similarWizardTemplate = document.querySelector('#similar-wizard-template').content.querySelector('.setup-similar-item');
 
 var NAMES = ['Иван', 'Хуан Себастьян', 'Мария', 'Кристоф', 'Виктор', 'Юлия', 'Люпита', 'Вашингтон'];
 var SURNAMES = ['да Марья', 'Верон', 'Мирабелла', 'Вальц', 'Онопко', 'Топольницкая', 'Нионго', 'Ирвинг'];
-var COAT_COLOR = ['rgb(101, 137, 164)', 'rgb(241, 43, 107)', 'rgb(146, 100, 161)', 'rgb(56, 159, 117)', 'rgb(215, 210, 55)', 'rgb(0, 0, 0)'];
-var EYES_COLOR = ['black', 'red', 'blue', 'yellow', 'green'];
+var COAT_COLORS = ['rgb(101, 137, 164)', 'rgb(241, 43, 107)', 'rgb(146, 100, 161)', 'rgb(56, 159, 117)', 'rgb(215, 210, 55)', 'rgb(0, 0, 0)'];
+var EYES_COLORS = ['black', 'red', 'blue', 'yellow', 'green'];
 
-var getRandomElement = function (arr) {
-  var rand = Math.floor(Math.random() * arr.length);
-  return arr[rand];
+setupElement.classList.remove('hidden');
+
+document.querySelector('.setup-similar').classList.remove('hidden');
+
+var getRandomElement = function (array) {
+  var random = Math.floor(Math.random() * array.length);
+  return array[random];
 };
 
 var getWizards = function (names, surnames, coatColors, eyesColors) {
@@ -37,12 +37,15 @@ var renderWizard = function (wizard) {
   return wizardElement;
 };
 
-var wizards = getWizards(NAMES, SURNAMES, COAT_COLOR, EYES_COLOR);
+var wizardsArray = getWizards(NAMES, SURNAMES, COAT_COLORS, EYES_COLORS);
 
-var fragment = document.createDocumentFragment();
-for (var i = 0; i < wizards.length; i++) {
-  fragment.appendChild(renderWizard(wizards[i]));
-}
+var getWizardsFragment = function (wizards) {
+  var fragment = document.createDocumentFragment();
+  for (var i = 0; i < wizards.length; i++) {
+    fragment.appendChild(renderWizard(wizards[i]));
+  }
 
-similarListElement.appendChild(fragment);
+  similarListElement.appendChild(fragment);
+};
 
+getWizardsFragment(wizardsArray);
